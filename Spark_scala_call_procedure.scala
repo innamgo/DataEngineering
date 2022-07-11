584a6f3828618efc67c02a4adc29758ad9e3baa4
Hoon Kim <innamgo@naver.com>
2022-07-11 11:30:28 +0900

    Update Spark_scala_call_procedure.scala

--- Spark_scala_call_procedure.scala
+++ Spark_scala_call_procedure.scala
@@ -1,20 +1,60 @@
-import java.sql.{Connection, DriverManager, ResultSet}
+import com.amazonaws.services.glue.ChoiceOption
+import com.amazonaws.services.glue.GlueContext
+import com.amazonaws.services.glue.MappingSpec
+import com.amazonaws.services.glue.ResolveSpec
+import com.amazonaws.services.glue.errors.CallSite
+import com.amazonaws.services.glue.util.GlueArgParser
+import com.amazonaws.services.glue.util.Job
+import com.amazonaws.services.glue.util.JsonOptions
+
+import org.apache.spark.SparkContext
+import scala.collection.JavaConverters._
+
+import org.apache.spark.sql._
+import org.apache.spark.sql.functions._
+
+import com.amazonaws.services.glue.log.GlueLogger
+
+import com.amazonaws.AmazonWebServiceRequest
+import com.amazonaws.services.secretsmanager._
+import com.amazonaws.services.secretsmanager.model._
 
 object GlueApp {
   def main(sysArgs: Array[String]) {
+    val spark: SparkContext = new SparkContext()
+    val glueContext: GlueContext = new GlueContext(spark)
+    // @params: [JOB_NAME]
+    val args = GlueArgParser.getResolvedOptions(sysArgs, Seq("JOB_NAME").toArray)
+    Job.init(args("JOB_NAME"), glueContext, args.asJava)
+   
+    val logger = new GlueLogger
+   
+    val client = AWSSecretsManagerClientBuilder.standard().build()
+   
+    val secretName = "secretName"
+    val getSecretValueRequest = new GetSecretValueRequest().withSecretId(secretName)
+    val getSecretValueResult = client.getSecretValue(getSecretValueRequest)
+    val secret = JsonOptions(getSecretValueResult.getSecretString()).toMap
+   
+    val jdbc_driver : String = "org.postgresql.Driver"
+    val jdbc_url : String = "jdbc:postgresql://" + secret("host") + ":" + secret("port") + "/" + secret("dbname")
+   
+    import java.sql.{Connection, DriverManager, ResultSet}
     classOf[org.postgresql.Driver]
-    val con_str = "jdbc:postgresql://172.30.1.6:49153/postgres?user=postgres&password=postgrespw"
-    val conn = DriverManager.getConnection(con_str)
+    val connection_string = "jdbc:postgresql://"+secret("host")+":"+secret("port")+"/"+secret("dbname")+"?user="+secret("username").toString+"&password="+secret("password").toString
+    val conn = DriverManager.getConnection(connection_string)
     try {
-      val stm = conn.createStatement(ResultSet.TYPE_FORWARD_ONLY, ResultSet.CONCUR_READ_ONLY)
-
-      val rs = stm.executeQuery("select 'aaaa' as ttt")
-      //stm.execute("call public.test_proc()")
-      while(rs.next) {
-        println(rs.getString("ttt"))
-        }
-      } finally {
+        val stm = conn.createStatement(ResultSet.TYPE_FORWARD_ONLY, ResultSet.CONCUR_READ_ONLY)
+        val rs = stm.execute("call pssetl.test()")
+        println(rs)
+    }
+    catch {
+        case ex: Exception => println(ex)
+    }
+    finally {
           conn.close()
-        }
+    }
+    Job.commit()
+    println("DONE.")
   }
 }
